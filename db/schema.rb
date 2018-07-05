b75fbe11be4fd746371f251ae0801af4f3398343
Ilya <ilya.krasnov@gmail.com>
2018-07-05 13:45:37 +0300

    Resolve schema.rb conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180704210256) do
-=======
 ActiveRecord::Schema.define(version: 20180704224831) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-=======
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -39,7 +35,6 @@ ActiveRecord::Schema.define(version: 20180704224831) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
->>>>>>> master
   end
 
 end
