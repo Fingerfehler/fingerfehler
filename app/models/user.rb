d2f74114e15d1e01cc6ac480e543ad10c935d0ef
Ben Kim <bhk1019@gmail.com>
2018-07-10 19:24:52 +0000

    added associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :games_as_white, :class_name => 'Game', :foreign_key => 'white_player_id'
+  has_many :games_as_black, :class_name => 'Game', :foreign_key => 'black_player_id'
 end
