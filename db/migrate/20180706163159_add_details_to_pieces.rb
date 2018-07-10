89754db8ab25d8bc96923b8f4c52bcaf520ec04a
Ben Kim <bhk1019@gmail.com>
2018-07-10 19:08:01 +0000

    added white? column to pieces migration

--- db/migrate/20180706163159_add_details_to_pieces.rb
+++ db/migrate/20180706163159_add_details_to_pieces.rb
@@ -2,6 +2,6 @@ class AddDetailsToPieces < ActiveRecord::Migration[5.0]
   def change
     add_column :x_coord, :y_coord, :integer
     add_column :type, :string
-    add_column :captured?, :boolean
+    add_column :captured?, :white?, :boolean
   end
 end
