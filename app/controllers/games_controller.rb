467ee0f9907fb2994a83f11bc7f44a4d9d85724b
saj528 <ajmarksboy@gmail.com>
2018-07-11 19:21:47 +0000

    First Draft

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,25 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+  end
+
   def new
     @game = Game.new
   end
 
   def create
+    @game = Game.create(game_params)
+    redirect_to root_path
   end
 
   def show
     #@game = Game.find(params[:id])
   end
 
+  private
+
+  def game_params
+    params.require(:game).permit(:name)
+  end
 
 end
