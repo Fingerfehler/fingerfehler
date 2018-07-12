5a58395c463f4d0fbbb6a20cc0428d87b79bb81d
brandongoodman615 <brandongoodman615@gmail.com>
2018-07-12 20:08:15 +0000

    Removed migrations for each piece type. Left the models with Piece inheritance

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180710211131) do
+ActiveRecord::Schema.define(version: 20180706163159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "bishops", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,21 +21,6 @@ ActiveRecord::Schema.define(version: 20180710211131) do
     t.string   "name"
     t.string   "state"
     t.integer  "turn"
-  end
-
-  create_table "kings", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "knights", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "pawns", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "pieces", force: :cascade do |t|
@@ -51,16 +31,6 @@ ActiveRecord::Schema.define(version: 20180710211131) do
     t.string   "type"
     t.boolean  "captured?"
     t.boolean  "white?"
-  end
-
-  create_table "queens", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "rooks", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
