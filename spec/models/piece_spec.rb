2558669981507dd842be23b8ebecfe615ad4c3a5
brandongoodman615 <brandongoodman615@gmail.com>
2018-07-17 18:48:01 +0000

    Added test, waiting for migration

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "is_vertically_obstructed?" do 
+    it "should find vertical obstructions" do
+      user = User.create()
+      game = Game.create(:name => "test", :white_player_id => user.id)
+      b_rook = Piece.create(:x_coord => 0, :y_coord => 0, :game => game.id)
+      obstructed_piece = Piece.new(:x_coord => 0, :y_coord => 1, :game =>game.id)
+      expect(b_rook.is_vertically_obstructed?(0,4)).to eq true
+    end
+  end
+
 end
