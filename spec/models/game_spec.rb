9a367a18f44a2b3b353b66cda35be9f72b080504
Ben Kim <bhk1019@gmail.com>
2018-07-18 18:00:41 +0000

    created sample test for populate_game!

---
+++ spec/models/game_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Game, type: :model do
+  describe "populate_game! method" do
+    it "should populate a new game after creation with 32 correctly placed pieces" do
+      
+    end
+  end
+end
