331f5b5c1573ac4f64341126b02e315dddabb143
Ben Kim <bhk1019@gmail.com>
2018-07-18 18:38:05 +0000

    wrote passing tests for populate_game, fixed schema, deleted pending tests

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
-  belongs_to :white_player, :class_name => 'User'
-  belongs_to :black_player, :class_name => 'User'
+  belongs_to :white_player, :class_name => 'User', optional: true
+  belongs_to :black_player, :class_name => 'User', optional: true
   has_many :pieces
   after_create :populate_game!
 
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
   describe "populate_game! method" do
-    it "should populate a new game after creation with 32 correctly placed pieces" do
-      
+    it "should populate a new game after creation with 32 pieces" do
+      game = Game.create
+      expect(game.pieces.length).to eq 32
+    end
+    it "should populate a new game with the queens at (3,0) and (3,7)" do
+      game = Game.create
+      w_queen = game.pieces.where(:type => 'Queen', :white? => true).first
+      b_queen = game.pieces.where(:type => 'Queen', :white? => false).first
+      expect([w_queen.x_coord, w_queen.y_coord]).to eq [3,0]
+      expect([b_queen.x_coord, b_queen.y_coord]).to eq [3,7]
     end
   end
 end
