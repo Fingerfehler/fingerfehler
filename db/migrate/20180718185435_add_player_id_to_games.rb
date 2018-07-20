f699808f0eafe6ac94b76d5376705629edf98937
ben kim <bhk1019@gmail.com>
2018-07-20 13:56:58 -0700

    deleted redundant player id migration file

--- db/migrate/20180718185435_add_player_id_to_games.rb
+++
@@ -1,6 +0,0 @@
-class AddPlayerIdToGames < ActiveRecord::Migration[5.0]
-  def change
-    add_column :games, :white_player_id, :integer
-    add_column :games, :black_player_id, :integer
-  end
-end
