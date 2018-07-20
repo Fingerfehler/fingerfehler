087ae11eaee96e365ad15f162098d17552e022bb
saj528 <ajmarksboy@gmail.com>
2018-07-20 19:41:58 +0000

    Show board loop refactoring fix

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,5 +1,9 @@
 module GamesHelper
   def black_white_cell(x,y)
-    (y+x).even? ? "white" : "black"
+    (y+x).even? ? "black" : "white"
+  end
+
+  def get_piece(pieces,x,y)
+    render html: '&#9817;'.html_safe
   end
 end
