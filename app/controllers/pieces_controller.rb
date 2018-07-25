de6a08bdbc0cde9cdb3602a0c04f0b44b3e0158a
saj528 <ajmarksboy@gmail.com>
2018-07-25 19:44:56 +0000

    moving

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -4,5 +4,11 @@ class PiecesController < ApplicationController
     @Piece = Piece.create(piece_params)
   end
 
+  def show
+    @piece = Piece.find_by_id(params[:x_coord && :y_coord])
+    @game = @piece.game
+    @pieces = [@piece]
+  end
+
 
 end
--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -1,2 +1,6 @@
 module PiecesHelper
+  def update_attributes(x, y)
+    @piece.x_coord = x
+    @piece.y_coord = y
+  end
 end
