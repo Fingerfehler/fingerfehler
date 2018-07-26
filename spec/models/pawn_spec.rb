357eea84643a21e55d28cd66d0368a4ad6e1d240
Ben Kim <bhk1019@gmail.com>
2018-07-26 02:04:09 +0000

    fixed missing bang in spec

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -10,14 +10,14 @@ RSpec.describe Pawn, type: :model do
       expect(w_pawn.valid_move?(2,3)).to eq true
       expect(b_pawn.valid_move?(3,5)).to eq true
       expect(b_pawn.valid_move?(3,4)).to eq true
-      w_pawn.move_to(2, 3)
-      b_pawn.move_to(3, 4)
+      w_pawn.move_to!(2, 3)
+      b_pawn.move_to!(3, 4)
       expect(w_pawn.valid_move?(2,4)).to eq true
       expect(w_pawn.valid_move?(2,5)).to eq false
       expect(b_pawn.valid_move?(3,3)).to eq true
       expect(b_pawn.valid_move?(3,2)).to eq false
       b_pawn2 = game.pieces.where(:x_coord => 2, :y_coord => 6).first
-      b_pawn2.move_to(2, 4)
+      b_pawn2.move_to!(2, 4)
       expect(w_pawn.valid_move?(2,4)).to eq false
       expect(w_pawn.valid_move?(3,4)).to eq true
     end
