eafbbee8c9a23caa1d8c9ffc77447091c996d451
Ben Kim <bhk1019@gmail.com>
2018-07-28 17:21:17 +0000

    refactored pawn valid move

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -9,17 +9,25 @@ class Pawn < Piece
   end
 
   def valid_move?(x,y)
-    if super(x,y)
-      movement_direction = white? ? 1 : -1
-      # standard movement
-      if x == x_coord && square_is_vacant?(x,y)
-        return true if (y == y_coord + movement_direction)
-        return true if (y == y_coord + (2 * movement_direction)) && self.has_not_moved?
-      end
-      # diagonal capture movement
-      return true if (x - x_coord).abs == 1 && (y == y_coord + movement_direction) && opponent_on_square?(x,y)
-    end
-    false
+    super(x,y) && (valid_one_forward_move?(x,y) || valid_two_forward_move?(x,y) || valid_standard_capture?(x,y))
   end
+
+  def valid_one_forward_move?(x,y)
+    square_is_vacant?(x,y) && x == x_coord && y = y_coord + self.movement_direction
+  end
+
+  def valid_two_forward_move?(x,y)
+    square_is_vacant?(x,y) && x == x_coord && y = y_coord + (self.movement_direction * 2)
+  end
+
+  def valid_standard_capture?(x,y)
+    (x - x_coord).abs == 1 && (y == y_coord + self.movement_direction) && opponent_on_square(x,y)
+  end
+
+  def movement_direction
+    white? ? 1 : -1
+  end
+
+
 
 end
