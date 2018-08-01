1c396ef7e93ac8875fcc2706530b3c16bae7dbe4
brandongoodman615 <brandongoodman615@gmail.com>
2018-08-01 01:13:53 +0000

    Queen valid_move with tests

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,4 +1,5 @@
 class Queen < Piece
+
   def get_image
     if white?
       '&#9813;'
@@ -6,4 +7,12 @@ class Queen < Piece
       '&#9819;'
     end
   end
+
+  def valid_move?(x,y)
+    return false unless super(x,y)
+    (y - y_coord).abs == (x - x_coord).abs ||
+    y == y_coord && x != x_coord || 
+    y != y_coord && x == x_coord
+  end  
+
 end
