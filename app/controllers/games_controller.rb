062eaa76d3e5f4c705194e0db17d8be413dbf06e
brandongoodman615 <brandongoodman615@gmail.com>
2018-08-19 18:41:57 +0000

    valid_works with JS

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
 class GamesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
 
   def index
     @gamesAll = Game.all
@@ -20,14 +21,15 @@ class GamesController < ApplicationController
     @game = Game.find(params[:game_id])
     @pieces = @game.pieces
     piece_to_move = Piece.find(params[:piece_id].to_i)
-    x = params[:x]
-    y = params[:y]
+    x = params[:x].to_i
+    y = params[:y].to_i
     if piece_to_move.valid_move?(x, y)
       piece_to_move.move_to!(x, y)
+      status = 202
     else
-
+      status = 422
     end
-    render :show
+    head status 
   end
 
   def new
