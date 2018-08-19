501108956a1e07ed32360904e23051bc5a43642f
brandongoodman615 <brandongoodman615@gmail.com>
2018-08-19 19:01:52 +0000

    fixed conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
     patch 'castle_kingside', to: 'games#castle_kingside', as: :castle_kingside
     patch 'castle_queenside', to: 'games#castle_queenside', as: :castle_queenside
   end
-  root 'games#index'
-  devise_for :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
